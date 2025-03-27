30ecb249a3b6df3269435ea734ef04fb6ac99c86
nathyyy06 <nathalia.cruz@estudante.ifms.edu.br>
2025-03-27 09:51:35 -0400

    Projeto/ptac-27-03

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 import { useState } from 'react';
+import { auth } from "./config/firebaseConfig.js";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
 
 function App() {
-  return <></>;
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  const autenticarComFirebase = async (event) => {
+      event.preventDefault();
+      try {
+          await signInWithEmailAndPassword(auth, email, senha);
+          alert("Login realizado com sucesso!");
+      } catch (error) {
+          alert("Erro ao autenticar: " + error.message);
+      }
+  };
+
+  return (
+    <div>
+        <h2>Login</h2>
+        <form onSubmit={autenticarComFirebase}>
+            <input
+                type="email"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+            />
+            <input
+                type="password"
+                placeholder="Senha"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
+            />
+            <button type="submit">Entrar</button>
+        </form>
+    </div>
+);
 }
+
 
 export default App;
